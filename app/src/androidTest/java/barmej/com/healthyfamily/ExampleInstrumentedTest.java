cae3f68ab4ab80dc06d6f6e5b99235c1b4ebb405
Abdullah Alhussainan <young.moby@gmail.com>
2020-06-09 14:02:17 +0300

    Update ExampleInstrumentedTest.java
    Androidx updates

--- app/src/androidTest/java/barmej/com/healthyfamily/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/barmej/com/healthyfamily/ExampleInstrumentedTest.java
@@ -2,13 +2,13 @@ package barmej.com.healthyfamily;
 
 import android.content.Context;
 
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,8 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("barmej.com.healthyfamily", appContext.getPackageName());
     }
 }
